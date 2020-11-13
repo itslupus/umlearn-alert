e4554bff0074eb6069d709ff5d9bae3ca5721b6d
Lucas <contact@itslucas.win>
2020-11-12 23:23:04 -0600

    Push spaces

--- bot.py
+++ bot.py
@@ -9,27 +9,27 @@ PREFIX = '.'
 
 TOKEN = None
 with open('token', 'r') as f:
-	TOKEN = f.read()
+    TOKEN = f.read()
 
 activity = discord.Game(name = 'version alpha')
 bot = discord.ext.commands.Bot(command_prefix = PREFIX, activity = activity, help_command = None)
 
 @bot.event
 async def on_connect():
-	print('Connected to Discord')
+    print('Connected to Discord')
 
 @bot.event
 async def on_ready():
-	bot.load_extension('commands')
-	SQLite()
+    bot.load_extension('commands')
+    SQLite()
 
-	print('Bot ready')
+    print('Bot ready')
 
 @bot.command()
 async def reload(context, module):
-	bot.reload_extension(module)
+    bot.reload_extension(module)
 
-	await context.channel.send('reloaded ' + module)
-	await context.message.delete()
+    await context.channel.send('reloaded ' + module)
+    await context.message.delete()
 
 bot.run(TOKEN)
