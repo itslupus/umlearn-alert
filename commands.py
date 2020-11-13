005f0253e1665ca87c9821dbec51ba66c0fe468a
Lucas <contact@itslucas.win>
2020-11-13 17:24:12 -0600

    Richard hates spaces

--- commands.py
+++ commands.py
@@ -6,33 +6,33 @@ from sqlite import SQLite
 CHANNEL = 484113924468244495
 
 class commands(discord.ext.commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-		self.sql = SQLite()
-		# self.output.start()
+    def __init__(self, bot):
+        self.bot = bot
+        self.sql = SQLite()
+        # self.output.start()
 
-	@discord.ext.commands.command(name = 'here')
-	async def here(self, context):
-		await context.channel.send('hello')
+    @discord.ext.commands.command(name = 'here')
+    async def here(self, context):
+        await context.channel.send('hello')
 
-	@discord.ext.commands.command(name = 'add')
-	async def add_course(self, context, *args):
-		if len(args) > 0 and len(args) < 3:
-			channel_id = args[1] if len(args) == 2 else context.channel.id
-			
-			if not self.bot.get_channel(channel_id) == None:
-				await context.channel.send('%s %s' % (args[0], channel_id))
-			else:
-				await context.channel.send('invalid channel %s' % (channel_id))
+    @discord.ext.commands.command(name = 'add')
+    async def add_course(self, context, *args):
+        if len(args) > 0 and len(args) < 3:
+            channel_id = args[1] if len(args) == 2 else context.channel.id
+            
+            if not self.bot.get_channel(channel_id) == None:
+                await context.channel.send('%s %s' % (args[0], channel_id))
+            else:
+                await context.channel.send('invalid channel %s' % (channel_id))
 
-		else:
-			await context.channel.send('invalid parameters for add')
+        else:
+            await context.channel.send('invalid parameters for add')
 
-		await context.message.delete()
-	
-	# @tasks.loop(seconds = 10)
-	# async def output(self):
-	# 	await self.bot.get_channel(CHANNEL).send('loop')
+        await context.message.delete()
+    
+    # @tasks.loop(seconds = 10)
+    # async def output(self):
+    # 	await self.bot.get_channel(CHANNEL).send('loop')
 
 def setup(bot):
-	bot.add_cog(commands(bot))
+    bot.add_cog(commands(bot))
--- sqlite.py
+++ sqlite.py
@@ -1,18 +1,18 @@
 import sqlite3
 
 class SQLite:
-	def __init__(self):
-		self.connection = sqlite3.connect('./database.db')
-		self.create_tables()
+    def __init__(self):
+        self.connection = sqlite3.connect('./database.db')
+        self.create_tables()
 
-	def create_tables(self):
-		cursor = self.connection.cursor()
-		cursor.execute('''
-			CREATE TABLE IF NOT EXISTS courses (
-				id INTEGER NOT NULL,
-				channel INTEGER NOT NULL,
-				last_check TIMESTAMP NOT NULL
-			)
-		''')
+    def create_tables(self):
+        cursor = self.connection.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS courses (
+                id INTEGER NOT NULL,
+                channel INTEGER NOT NULL,
+                last_check TIMESTAMP NOT NULL
+            )
+        ''')
 
-		self.connection.commit()
+        self.connection.commit()
